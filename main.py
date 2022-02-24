ab2d2a5aed604c2385560f6d08bb81b87d3015a7
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-25 02:46:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -48,10 +48,19 @@ async def thumb_change(bot, m):
     msg = await m.reply("`Downloading..`", parse_mode='md')
     c_time = time.time()
     file_dl_path = await bot.download_media(message=m, progress=progress_for_pyrogram, progress_args=("Downloading file..", msg, c_time))
-
-    answer = await bot.ask(m.chat.id,'Send the thumbnail' + ' or /keep to keep the previous thumb' if thumb, filters=filters.photo | filters.text)
-
-    
+    await msg.delete()
+    answer = await bot.ask(m.chat.id,'Now send the thumbnail' + ' or /keep to keep the previous thumb' if thumb, filters=filters.photo | filters.text)
+    if answer.photo:
+        os.remove(thumb)
+        thumb = await bot.download_media(message=answer.photo)
+    msg = await m.reply("`Uploading..`", parse_mode='md')
+    c_time = time.time()
+    if m.document:
+        await bot.send_document(chat_id=m.chat.id, document=file_dl_path, thumb=thumb, caption=m.caption if m.caption else None, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
+    elif m.video:
+        await bot.send_video(chat_id=m.chat.id, video=file_dl_path, thumb=thumb, caption=m.caption if m.caption else None, progress=progress_for_pyrogram, progress_args=("Uploading file..", msg, c_time))
+    await msg.delete()
+    os.remove(file_dl_path)
 
 
 
