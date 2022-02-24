e48df4071bfe35cd5fc1df7d81824cfc1592cff9
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-25 02:23:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,9 @@ async def thumb_change(bot, m):
     global thumb
     msg = await m.reply("`Downloading..`", parse_mode='md')
     c_time = time.time()
-    file_dl_path = await bot.download_media(message=m, file_name="temp/vid.mp4", progress=progress_for_pyrogram, progress_args=("Downloading file..", msg, c_time))
+    file_dl_path = await bot.download_media(message=m, progress=progress_for_pyrogram, progress_args=("Downloading file..", msg, c_time))
+
+    answer = await bot.ask(m.chat.id,'Send the thumbnail' + ' or /keep to keep the previous thumb' if thumb, filters=filters.photo | filters.text)
 
     
 
